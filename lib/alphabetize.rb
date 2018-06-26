f0197dcff799bae07e0e0f0187ebc50ab6bf7555
Yicheng Jin <bobjin42@gmail.com>
2018-06-26 22:25:45 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,11 +2,15 @@ def alphabetize(arr)
   # code here
   list  = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
   
-  arr.sort_by{|sentence|
+  new_arr = arr.sort_by{|sentence|
     sentence.split(" ").sort_by{|word|
       word.split("").sort_by{|char|
         list.index(char)
       }
     } 
+  }
+  
+  new_arr.sort_by{|sentence
+    list.index(sentence[0])
   }
 end
