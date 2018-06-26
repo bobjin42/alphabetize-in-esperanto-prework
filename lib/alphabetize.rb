6bca31844df53376660259e275284195f454f8d1
Yicheng Jin <bobjin42@gmail.com>
2018-06-26 22:05:08 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,8 +2,7 @@ def alphabetize(arr)
   # code here
   list  = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
   arr.sort_by{|sentence|
-    char = sentence.split("")
-    list.index(char)
+    list.index(sentence[0])
     } 
 
 end
